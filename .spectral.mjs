0f70a043acbeeda9f960656796d57179a0cb199f
qa-gqglt-1-nzcr@example.com <qa-gqglt-1-nzcr@example.com>
2023-02-08 19:07:11 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjEwMjA3NQ/spectral.js?branch_slug=main&token=161db82e-7b86-46ff-9a69-435738b169b5";
+export default { extends: ruleset };
